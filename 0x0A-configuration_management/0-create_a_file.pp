a4fe5fa87c7a791272cd60793bdff1757540fb05
birukmak10 <birukmekonen10@gmail.com>
2023-06-28 12:14:47 +0300

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create school resource inthe /tmp dir
+file { 'resource title':
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
